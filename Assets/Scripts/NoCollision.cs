1916877fa29fd932a3c4ba85557b0e591f5c25f3
jacobHancock0402 <goddhoward0402@gmail.com>
2020-10-25 22:23:18 +0000

    all code

---
+++ Assets/Scripts/NoCollision.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ 
+public class NoCollision : MonoBehaviour
+{
+    // Start is called before the first frame update
+ 
+ 
+ 
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if ((coll.gameObject.tag == "Player" || coll.gameObject.tag == "rArm" || coll.gameObject.tag == "Gun" || coll.gameObject.tag == "lLeg" || coll.gameObject.tag == "rLeg" || coll.gameObject.tag == "LlLeg" || coll.gameObject.tag == "LrLeg") && (coll.gameObject.tag != gameObject.tag))
+        {
+            Physics2D.IgnoreCollision(this.gameObject.GetComponent<Collider2D>(), coll.gameObject.GetComponent<Collider2D>());
+        }
+        
+    }
+}
